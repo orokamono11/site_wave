1db5c0dcf997ddaba97cd5a91df6d86107d03598
namakemono <namakemono@localhost.localdomain>
2017-08-15 21:03:52 +0900

    centosでの開発

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 1000.times do |d|
-#     Site.create(
-#         user_id: 1,
-#         title: "サイト#{d}",
-#         address: "site#{d}.com",
-#         introduction: "紹介文#{d}"
-#     )
-# end
+1000.times do |d|
+    Site.create(
+        user_id: 1,
+        title: "サイト#{d}",
+        address: "site#{d}.com",
+        introduction: "紹介文#{d}"
+    )
+end
 
-# User.create(user_name:  "girujinia",
-#              email: "girujinia@gmail.com",
-#              password:              "sgirujinia",
-#              password_confirmation: "sgirujinia")
+User.create(user_name:  "girujinia",
+             email: "girujinia@gmail.com",
+             password:              "sgirujinia",
+             password_confirmation: "sgirujinia")
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
